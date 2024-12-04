93f13abc7bfaba0b89427c24a3a659f7de717c10
ebuka nathan <ebukanathan@gmail.com>
2024-12-04 23:50:51 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex gap-2 ">
+        <div className="w-[450px] bg-yellow-200">Profile</div>
+        <div className="w-[400px] bg-lime-300">Budget Vs Expenses</div>
+        <div className="w-[400px] bg-cyan-400"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
